2c38c9b3d8d8246ad7eea75de13dc391c5ba186d
BBJ Digital <dev@bbjdigital.com>
2026-03-05 17:14:58 +0000

    Remove Layout wrapper from admin dashboard to hide navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -57,7 +57,7 @@ export default function App() {
         path="/admin"
         element={
           <ProtectedRoute requiredRole="admin">
-            <Layout><AdminDashboard /></Layout>
+            <AdminDashboard />
           </ProtectedRoute>
         }
       />
